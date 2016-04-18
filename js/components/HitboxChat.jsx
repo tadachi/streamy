c469d511c5ebaaf1b47d19844ebb63de7a0988cd
Takumi Adachi <takumiadachi@gmail.com>
2016-04-17 18:45:51 -0700

    Added Hitbox Chat file.

---
+++ js/components/HitboxChat.jsx
@@ -0,0 +1,12 @@
+var HitboxChat = React.createClass({
+    getInitialState: function() {
+        return {
+            width: null,
+            height: null,
+            channel: null,
+        };
+    },
+    
+});
+
+module.exports = HitboxChat;
---
+++ js/components/HitboxPlayer.jsx
@@ -0,0 +1,13 @@
+var HitboxPlayer = React.createClass({
+
+    getInitialState: function() {
+        return {
+            width: null,
+            height: null,
+            channel: null,
+        };
+    },
+
+});
+
+module.exports = HitboxPlayer;
