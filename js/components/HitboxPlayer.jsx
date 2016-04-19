f1307708bffdb0ab7cadc95fb94fb9391ced5656
Takumi Adachi <takumiadachi@gmail.com>
2016-04-18 17:52:45 -0700

    Hitbox video player code.

--- js/components/HitboxPlayer.jsx
+++ js/components/HitboxPlayer.jsx
@@ -1,3 +1,6 @@
+var $ = require('jquery');
+var GLOBALS = require('../GLOBALS.js');
+
 var HitboxPlayer = React.createClass({
 
     getInitialState: function() {
@@ -6,6 +9,46 @@ var HitboxPlayer = React.createClass({
             height: null,
             channel: null,
         };
+    },
+
+    setWidth: function(width) {
+        $('#' + this.props.div_id).css('width', width);
+    },
+
+    setHeight: function(height) {
+        $('#' + this.props.div_id).css('height', height);
+    },
+
+    setChannel: function(channel) {
+        this.setState({channel: channel});
+        this.removeIframeChat();
+    },
+
+    loadVideoPlayer: function() {
+        this.setState({flex_div: 'none'});
+
+        var src = 'https://www.hitbox.tv/#!/embed/{CHANNEL}?autoplay=true'.format({
+			CHANNEL: this.state.channel});
+
+		html =    ['<iframe width="{w}" '.format({ w: center.w }),
+						'height="{h}" '.format({ h: center.h }),
+						'id={div_id}',
+						'src="{src}" '.format({ src: src }),
+						'frameborder="0 ',
+						'allowfullscreen',
+						'>',
+					'</iframe>'
+					].join('');
+
+        $('#' + this.props.parent_div_id).prepend(html);
+
+    },
+
+    removeIframeVideo: function() {
+        var iframe = $('#' + this.props.div_id).get(0);
+        if (iframe) {
+            iframe.parentNode.removeChild(iframe);
+        }
     },
 
 });
