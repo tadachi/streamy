e7a91f7f2128cb787a2f5c983ad2a2194f8574bc
Takumi Adachi <takumiadachi@gmail.com>
2016-04-20 17:37:50 -0700

    Added some boilerplate to HitboxChat.

--- js/components/HitboxChat.jsx
+++ js/components/HitboxChat.jsx
@@ -7,6 +7,45 @@ var HitboxChat = React.createClass({
         };
     },
     
+    setWidth: function(width) {
+        $('#' + this.props.div_id).css('width', width);
+    },
+
+    setHeight: function(height) {
+        $('#' + this.props.div_id).css('height', height);
+    },
+
+    setChatChannel: function(channel) {
+        this.removeIframeChat();
+    },    
+    
+    loadChat: function() {
+        this.setState({flex_div: 'none'});
+
+        // Change the chat to the corresponding video channel.
+        var src = 'https://www.twitch.tv/{CHANNEL}/chat'.format({
+            CHANNEL: this.state.channel});
+
+		var html =    ['<iframe ',
+						'id="chat" ',
+						'frameborder="0" ',
+						'scrolling="yes" ',
+                        'src="{src}" '.format({src: src}),
+                        'width="{w}" '.format({w: this.state.width}),
+                        'height="{h}">'.format({h: this.state.height}),
+					'</iframe>'].join("");
+
+        $('#' + this.props.parent_div_id).prepend(html);
+
+    },
+    
+    removeIframeChat: function() {
+        var iframe = $('#' + this.props.div_id).get(0);
+        if (iframe) {
+            iframe.parentNode.removeChild(iframe);
+        }
+    },
+    
 });
 
 module.exports = HitboxChat;
