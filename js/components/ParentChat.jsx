3088037f7ffbba50baf49c9c184c84a4a81417ec
Takumi Adachi <takumiadachi@gmail.com>
2016-04-26 16:47:03 -0700

    Added a ParentChat class to wrap Twitch and Hitbox chat.

---
+++ js/components/ParentChat.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var $ = require('jquery');
+var GLOBALS = require('../GLOBALS.js');
+
+var ParentChat = React.createClass({
+
+    getInitialState: function() {
+        return {
+            width: null,
+            height: null,
+            channel: null,
+        };
+    },
+        
+})
+
+module.exports = ParentChat;
